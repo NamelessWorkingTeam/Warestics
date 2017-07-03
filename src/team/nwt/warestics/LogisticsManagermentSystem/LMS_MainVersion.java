2d44129ca0f58a4b1a46dc7d4899a514dbabf6db
Polaris_Sun <polaris_Sun@foxmail.com>
2017-07-03 13:33:38 +0800

    界面

--- src/team/nwt/warestics/LogisticsManagermentSystem/LMS_MainVersion.java
+++ src/team/nwt/warestics/LogisticsManagermentSystem/LMS_MainVersion.java
@@ -6,10 +6,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class LMS_MainVersion extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -32,11 +38,55 @@ public class LMS_MainVersion extends JFrame {
 	 */
 	public LMS_MainVersion() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 641, 435);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.WEST);
+		
+		textField = new JTextField();
+		panel_1.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		panel_1.add(btnNewButton_1);
+		
+		JPanel panel_2 = new JPanel();
+		contentPane.add(panel_2, BorderLayout.EAST);
+		
+		JButton btnNewButton = new JButton("New button");
+		panel_2.add(btnNewButton);
+		
+		JPanel panel_3 = new JPanel();
+		contentPane.add(panel_3, BorderLayout.NORTH);
+		
+		JLabel label = new JLabel("物流管理系统");
+		panel_3.add(label);
+		
+		JPanel panel_4 = new JPanel();
+		contentPane.add(panel_4, BorderLayout.SOUTH);
+		
+		JButton button_1 = new JButton("添加");
+		panel_4.add(button_1);
+		
+		JButton button_2 = new JButton("删除");
+		panel_4.add(button_2);
+		
+		JButton button_3 = new JButton("修改");
+		panel_4.add(button_3);
+		
+		JButton button = new JButton("退出");
+		panel_4.add(button);
 	}
 
 }

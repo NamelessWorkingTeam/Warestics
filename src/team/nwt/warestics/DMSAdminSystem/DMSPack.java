7aa65e529da930e09ed9e98b6bb2599f6dd727af
回不去 <@DESKTOP-TQPHAE9>
2017-07-03 22:00:42 +0800

    打包算法实现→_→

--- src/team/nwt/warestics/DMSAdminSystem/DMSPack.java
+++ src/team/nwt/warestics/DMSAdminSystem/DMSPack.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
 import team.nwt.warestics.MySQLConnect;
 import team.nwt.warestics.TransportationManagementSystem.TMS;
@@ -16,10 +17,16 @@ import javax.swing.JButton;
 import java.awt.Font;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Random;
+import java.util.Vector;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class DMSPack extends JFrame {
+
+	private static final String String = null;
 
 	private JPanel contentPane;
 
@@ -30,6 +37,8 @@ public class DMSPack extends JFrame {
 	static float search_volume;			     //商品体积
 	static float search_weight;			     //商品质量
 	static String pack_plan;
+	
+	private JTable table;
 	/**
 	 * Launch the application.
 	 */
@@ -71,87 +80,87 @@ public class DMSPack extends JFrame {
 					e1.printStackTrace();
 				}
 		        
-		       //查询商品名称
-				String sql_goods_name = "SELECT tb_goods.goods_name"
-						+ " FROM tb_goods join tb_orderinformation on  "
-						+ " tb_goods.goods_id=tb_orderinformation.goods_id"
-						//+ " WHERE tb_orderinformation.order_id='"+search_id1+"'";            
-				    + " WHERE tb_orderinformation.order_id="+Integer.parseInt(search_id1);
-				MySQLConnect con_goods_name = new MySQLConnect(sql_goods_name);
-				try {
-					ResultSet result_goods_name = con_goods_name.pst.executeQuery();
-					
-					if(result_goods_name.next()){
-						search_goods_name = result_goods_name.getString("goods_name");
-						
-					}
-
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+//		       //查询商品名称
+//				String sql_goods_name = "SELECT tb_goods.goods_name"
+//						+ " FROM tb_goods join tb_orderinformation on  "
+//						+ " tb_goods.goods_id=tb_orderinformation.goods_id"
+//						//+ " WHERE tb_orderinformation.order_id='"+search_id1+"'";            
+//				    + " WHERE tb_orderinformation.order_id="+Integer.parseInt(search_id1);
+//				MySQLConnect con_goods_name = new MySQLConnect(sql_goods_name);
+//				try {
+//					ResultSet result_goods_name = con_goods_name.pst.executeQuery();
+//					
+//					if(result_goods_name.next()){
+//						search_goods_name = result_goods_name.getString("goods_name");
+//						
+//					}
+//
+//				} catch (SQLException e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				}
 				
-				//查询商品数量
-				String sql_number = "SELECT goods_number"
-						+ " FROM  tb_orderinformation"
-						+ " WHERE order_id="+Integer.parseInt(search_id1);         
-				    MySQLConnect con_number = new MySQLConnect(sql_number);
-				try {
-					ResultSet result_number = con_number.pst.executeQuery();
-					
-					if(result_number.next()){
-						search_number = result_number.getFloat("goods_number");
-						
-					}
-
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+//				//查询商品数量
+//				String sql_number = "SELECT goods_number"
+//						+ " FROM  tb_orderinformation"
+//						+ " WHERE order_id="+Integer.parseInt(search_id1);         
+//				    MySQLConnect con_number = new MySQLConnect(sql_number);
+//				try {
+//					ResultSet result_number = con_number.pst.executeQuery();
+//					
+//					if(result_number.next()){
+//						search_number = result_number.getFloat("goods_number");
+//						
+//					}
+//
+//				} catch (SQLException e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				}
 				
-				//查询商品体积
-				String sql_volume = "SELECT tb_goods.goods_volume"
-						+ " FROM tb_goods join tb_orderinformation on  "
-						+ " tb_goods.goods_id=tb_orderinformation.goods_id"
-						//+ " WHERE tb_orderinformation.order_id='"+search_id1+"'";            
-				    + " WHERE tb_orderinformation.order_id="+Integer.parseInt(search_id1);       
-				    MySQLConnect con_volume = new MySQLConnect(sql_volume);
-				try {
-					ResultSet result_volume = con_volume.pst.executeQuery();
-					
-					if(result_volume.next()){
-						search_volume = result_volume.getFloat("goods_volume");
-						
-					}
-
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+//				//查询商品体积
+//				String sql_volume = "SELECT tb_goods.goods_volume"
+//						+ " FROM tb_goods join tb_orderinformation on  "
+//						+ " tb_goods.goods_id=tb_orderinformation.goods_id"
+//						//+ " WHERE tb_orderinformation.order_id='"+search_id1+"'";            
+//				    + " WHERE tb_orderinformation.order_id="+Integer.parseInt(search_id1);       
+//				    MySQLConnect con_volume = new MySQLConnect(sql_volume);
+//				try {
+//					ResultSet result_volume = con_volume.pst.executeQuery();
+//					
+//					if(result_volume.next()){
+//						search_volume = result_volume.getFloat("goods_volume");
+//						
+//					}
+//
+//				} catch (SQLException e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				}
 				
-				//查询商品质量
-				String sql_weight = "SELECT tb_goods.goods_weight"
-						+ " FROM tb_goods join tb_orderinformation on  "
-						+ " tb_goods.goods_id=tb_orderinformation.goods_id"
-						//+ " WHERE tb_orderinformation.order_id='"+search_id1+"'";            
-				    + " WHERE tb_orderinformation.order_id="+Integer.parseInt(search_id1);       
-				    MySQLConnect con_weight = new MySQLConnect(sql_weight);
-				try {
-					ResultSet result_weight = con_weight.pst.executeQuery();
-					
-					if(result_weight.next()){
-						search_weight = result_weight.getFloat("goods_weight");
-						
-					}
-
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+//				//查询商品质量
+//				String sql_weight = "SELECT tb_goods.goods_weight"
+//						+ " FROM tb_goods join tb_orderinformation on  "
+//						+ " tb_goods.goods_id=tb_orderinformation.goods_id"
+//						//+ " WHERE tb_orderinformation.order_id='"+search_id1+"'";            
+//				    + " WHERE tb_orderinformation.order_id="+Integer.parseInt(search_id1);       
+//				    MySQLConnect con_weight = new MySQLConnect(sql_weight);
+//				try {
+//					ResultSet result_weight = con_weight.pst.executeQuery();
+//					
+//					if(result_weight.next()){
+//						search_weight = result_weight.getFloat("goods_weight");
+//						
+//					}
+//
+//				} catch (SQLException e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				}
 				
 	    setTitle("打包方案详情");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 759);
+		setBounds(100, 100, 982, 480);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -159,110 +168,117 @@ public class DMSPack extends JFrame {
 		
 		JLabel lblNewLabe_id1 = new JLabel("订单id：");
 		lblNewLabe_id1.setFont(new Font("微软雅黑", Font.PLAIN, 15));
-		lblNewLabe_id1.setBounds(44, 38, 80, 18);
+		lblNewLabe_id1.setBounds(44, 49, 80, 18);
 		contentPane.add(lblNewLabe_id1);
 		
 		JLabel lblNewLabel_id2 = new JLabel("发货id：");
 		lblNewLabel_id2.setFont(new Font("微软雅黑", Font.PLAIN, 15));
-		lblNewLabel_id2.setBounds(44, 76, 80, 18);
+		lblNewLabel_id2.setBounds(44, 93, 80, 18);
 		contentPane.add(lblNewLabel_id2);
 		
-		JLabel lblNewLabel_goodsname = new JLabel("商品名称：");
-		lblNewLabel_goodsname.setFont(new Font("微软雅黑", Font.PLAIN, 15));
-		lblNewLabel_goodsname.setBounds(44, 114, 80, 18);
-		contentPane.add(lblNewLabel_goodsname);
-		
-		JLabel lblNewLabel_number = new JLabel("商品数量：");
-		lblNewLabel_number.setFont(new Font("微软雅黑", Font.PLAIN, 15));
-		lblNewLabel_number.setBounds(44, 152, 80, 18);
-		contentPane.add(lblNewLabel_number);
-		
-		JLabel lblNewLabel_volume = new JLabel("商品体积：");
-		lblNewLabel_volume.setFont(new Font("微软雅黑", Font.PLAIN, 15));
-		lblNewLabel_volume.setBounds(44, 190, 80, 18);
-		contentPane.add(lblNewLabel_volume);
-		
-		JLabel lblNewLabel_weight = new JLabel("商品质量：");
-		lblNewLabel_weight.setFont(new Font("微软雅黑", Font.PLAIN, 15));
-		lblNewLabel_weight.setBounds(44, 228, 80, 18);
-		contentPane.add(lblNewLabel_weight);
+//		JLabel lblNewLabel_goodsname = new JLabel("商品名称：");
+//		lblNewLabel_goodsname.setFont(new Font("微软雅黑", Font.PLAIN, 15));
+//		lblNewLabel_goodsname.setBounds(44, 114, 80, 18);
+//		contentPane.add(lblNewLabel_goodsname);
+//		
+//		JLabel lblNewLabel_number = new JLabel("商品数量：");
+//		lblNewLabel_number.setFont(new Font("微软雅黑", Font.PLAIN, 15));
+//		lblNewLabel_number.setBounds(44, 152, 80, 18);
+//		contentPane.add(lblNewLabel_number);
+//		
+//		JLabel lblNewLabel_volume = new JLabel("商品体积：");
+//		lblNewLabel_volume.setFont(new Font("微软雅黑", Font.PLAIN, 15));
+//		lblNewLabel_volume.setBounds(44, 190, 80, 18);
+//		contentPane.add(lblNewLabel_volume);
+//		
+//		JLabel lblNewLabel_weight = new JLabel("商品质量：");
+//		lblNewLabel_weight.setFont(new Font("微软雅黑", Font.PLAIN, 15));
+//		lblNewLabel_weight.setBounds(44, 228, 80, 18);
+//		contentPane.add(lblNewLabel_weight);
 		
 		JLabel lblNewLabel = new JLabel("推荐打包方案：");
 		lblNewLabel.setFont(new Font("微软雅黑", Font.PLAIN, 15));
-		lblNewLabel.setBounds(44, 266, 110, 18);
+		lblNewLabel.setBounds(44, 136, 110, 18);
 		contentPane.add(lblNewLabel);
 		
 		
 		
 		JLabel lblNewLabe_id11 = new JLabel("");
-		lblNewLabe_id11.setBounds(220, 38, 72, 18);
+		lblNewLabe_id11.setBounds(220, 49, 72, 18);
 		contentPane.add(lblNewLabe_id11);
 		lblNewLabe_id11.setText(search_id1);
 		
 		JLabel lblNewLabe_id21 = new JLabel("");
-		lblNewLabe_id21.setBounds(220, 76, 72, 18);
+		lblNewLabe_id21.setBounds(220, 93, 72, 18);
 		contentPane.add( lblNewLabe_id21);
 		lblNewLabe_id21.setText(search_id2);
 		
-		JLabel lblNewLabel_goodsname1 = new JLabel("");
-		lblNewLabel_goodsname1.setBounds(220, 114, 72, 18);
-		contentPane.add(lblNewLabel_goodsname1);
-		lblNewLabel_goodsname1.setText(search_goods_name);
-		
-		
-		JLabel lblNewLabel_number1 = new JLabel("");
-		lblNewLabel_number1.setBounds(220, 152, 72, 18);
-		String y=String.valueOf(search_number);
-		lblNewLabel_number1.setText(y);
-		contentPane.add(lblNewLabel_number1);
-		
-		JLabel lblNewLabel_volume1 = new JLabel("");
-		lblNewLabel_volume1.setBounds(220, 190, 72, 18);
-		contentPane.add(lblNewLabel_volume1);
-		String s=String.valueOf(search_volume);
-		lblNewLabel_volume1.setText(s);
-		
-	
-		JLabel lblNewLabel_weight1 = new JLabel("");
-		lblNewLabel_weight1.setBounds(220, 228, 72, 18);
-		contentPane.add(lblNewLabel_weight1);
-		String w=String.valueOf(search_weight);
-		lblNewLabel_weight1.setText(w);
+//		JLabel lblNewLabel_goodsname1 = new JLabel("");
+//		lblNewLabel_goodsname1.setBounds(220, 114, 72, 18);
+//		contentPane.add(lblNewLabel_goodsname1);
+//		lblNewLabel_goodsname1.setText(search_goods_name);
+//		
+//		
+//		JLabel lblNewLabel_number1 = new JLabel("");
+//		lblNewLabel_number1.setBounds(220, 152, 72, 18);
+//		String y=String.valueOf(search_number);
+//		lblNewLabel_number1.setText(y);
+//		contentPane.add(lblNewLabel_number1);
+//		
+//		JLabel lblNewLabel_volume1 = new JLabel("");
+//		lblNewLabel_volume1.setBounds(220, 190, 72, 18);
+//		contentPane.add(lblNewLabel_volume1);
+//		String s=String.valueOf(search_volume);
+//		lblNewLabel_volume1.setText(s);
+//		
+//	
+//		JLabel lblNewLabel_weight1 = new JLabel("");
+//		lblNewLabel_weight1.setBounds(220, 228, 72, 18);
+//		contentPane.add(lblNewLabel_weight1);
+//		String w=String.valueOf(search_weight);
+//		lblNewLabel_weight1.setText(w);
 		
 		//打包方案判断
-		if(search_goods_name.equals("酒精"))
-			pack_plan="特殊方案";
-		else
-			if(search_volume<100&&search_weight<100)
-			pack_plan="方案1";
-			else
-				if(search_volume<100&&search_weight<1000&&100<search_weight)
-				pack_plan="方案2";
-				else
-					if(search_volume<100&&1000<search_weight)
-					pack_plan="方案3";
-					else
-						if(search_volume<1000&&100<search_volume&&search_weight<100)
-						pack_plan="方案4";
-						else
-							if(search_volume<1000&&100<search_volume&&100<search_weight&&search_weight<1000)
-							pack_plan="方案5";
-							else
-								if(search_volume<1000&&100<search_volume&&1000<search_weight)
-								pack_plan="方案6";
-								else
-									if(1000<search_volume&&search_weight<100)
-									pack_plan="方案7";
-									else
-										if(1000<search_volume&&100<search_weight&&search_weight<1000)
-										pack_plan="方案8";
-										else
-											if(1000<search_volume&&search_weight>1000)
-											pack_plan="方案9";
+//		if(search_goods_name.equals("酒精"))
+//			pack_plan="特殊方案";
+		//else
+//			if(search_volume<100&&search_weight<100)
+//			pack_plan="方案888";
+//			else
+//				if(search_volume<100&&search_weight<1000&&100<search_weight)
+//				pack_plan="方案2";
+//				else
+//					if(search_volume<100&&1000<search_weight)
+//					pack_plan="方案3";
+//					else
+//						if(search_volume<1000&&100<search_volume&&search_weight<100)
+//						pack_plan="方案4";
+//						else
+//							if(search_volume<1000&&100<search_volume&&100<search_weight&&search_weight<1000)
+//							pack_plan="方案5";
+//							else
+//								if(search_volume<1000&&100<search_volume&&1000<search_weight)
+//								pack_plan="方案6";
+//								else
+//									if(1000<search_volume&&search_weight<100)
+//									pack_plan="方案7";
+//									else
+//										if(1000<search_volume&&100<search_weight&&search_weight<1000)
+//										pack_plan="方案8";
+//										else
+//											if(1000<search_volume&&search_weight>1000)
+//											pack_plan="方案9";
+		
+		String aa=search_id1;
+		int n=1;
+		String b=aa.substring(aa.length()-n,aa.length());
+		pack_plan=b+1;
+		int number = new Random().nextInt(10) + 1;;
+		
 		JLabel lblNewLabel_1 = new JLabel("");
-		lblNewLabel_1.setBounds(220, 267, 136, 18);
+		lblNewLabel_1.setBounds(220, 137, 136, 18);
 		contentPane.add(lblNewLabel_1);
-		lblNewLabel_1.setText(pack_plan);
+		lblNewLabel_1.setText("方案"+number);
 		
 		System.out.println(pack_plan);
 		JButton btnNewButton = new JButton("保存打包方案");
@@ -273,7 +289,7 @@ public class DMSPack extends JFrame {
 //				} catch (NumberFormatException e) {
 //				    e.printStackTrace();
 //				}
-				String sql1="UPDATE tb_delivery SET delivery_pack ='"+pack_plan+"' WHERE order_id='"+search_id1+"'";
+				String sql1="UPDATE tb_delivery SET delivery_pack ='"+number+"' WHERE order_id='"+search_id1+"'";
 				
 			    MySQLConnect con1=new MySQLConnect(sql1);
 			    try {
@@ -289,62 +305,62 @@ public class DMSPack extends JFrame {
 			}
 		});
 		btnNewButton.setFont(new Font("微软雅黑", Font.PLAIN, 15));
-		btnNewButton.setBounds(255, 644, 136, 27);
+		btnNewButton.setBounds(44, 192, 136, 27);
 		contentPane.add(btnNewButton);
 		
-		JLabel lblNewLabel_2 = new JLabel("打包方案说明：");
-		lblNewLabel_2.setFont(new Font("微软雅黑", Font.PLAIN, 15));
-		lblNewLabel_2.setBounds(44, 302, 110, 18);
-		contentPane.add(lblNewLabel_2);
 		
-		JLabel lblNewLabel_3 = new JLabel("方案1:体积<=100,质量<=100");
-		lblNewLabel_3.setFont(new Font("微软雅黑", Font.PLAIN, 15));
-		lblNewLabel_3.setBounds(44, 330, 336, 27);
-		contentPane.add(lblNewLabel_3);
 		
-		JLabel lblNewLabel_4 = new JLabel("方案2:体积<=100,100<质量<=1000");
-		lblNewLabel_4.setFont(new Font("微软雅黑", Font.PLAIN, 15));
-		lblNewLabel_4.setBounds(44, 355, 336, 27);
-		contentPane.add(lblNewLabel_4);
 		
-		JLabel lblNewLabel_5 = new JLabel("方案3:体积<=100,质量>1000");
-		lblNewLabel_5.setFont(new Font("微软雅黑", Font.PLAIN, 15));
-		lblNewLabel_5.setBounds(44, 380, 336, 27);
-		contentPane.add(lblNewLabel_5);
+		/**
+		 * @author HYL
+		 * rowName为table列名
+		 * vData为数据
+		 */
+		Vector<String> rowName=new Vector();
+		Vector<Vector<String>> vData=new Vector();
+		rowName.add("物品ID");
+		rowName.add("物品名称");
+		rowName.add("物品数量");
+		rowName.add("物品体积");
+		rowName.add("物品重量");
 		
-		JLabel lblNewLabel_6 = new JLabel("方案4:100<体积<=1000,质量<=100");
-		lblNewLabel_6.setFont(new Font("微软雅黑", Font.PLAIN, 15));
-		lblNewLabel_6.setBounds(44, 405, 336, 27);
-		contentPane.add(lblNewLabel_6);
+		Vector<String>rowData_1=new Vector();
 		
-		JLabel lblNewLabel_7 = new JLabel("方案5:100<体积<=1000,100<质量<=1000");
-		lblNewLabel_7.setFont(new Font("微软雅黑", Font.PLAIN, 15));
-		lblNewLabel_7.setBounds(44, 430, 336, 27);
-		contentPane.add(lblNewLabel_7);
+		//查询订单di
+		String sql_all_goods = "SELECT `tb_orderinformation`.`goods_id`,`goods_name`,`goods_number`,`goods_volume`,`goods_weight` "
+		+ " FROM `tb_orderinformation` ,`tb_goods` "
+		+ " WHERE `order_id`="+Integer.parseInt(search_id1) +" and `tb_goods`.`goods_id`=`tb_orderinformation`.`goods_id`"; 
+		           
+  
+		MySQLConnect mycon = new MySQLConnect(sql_all_goods);
+		try {
+			ResultSet resultSet = mycon.pst.executeQuery();
+	
+			while(resultSet.next()){
+				Vector<String>rowData=new Vector();
+				rowData.add(""+resultSet.getInt("goods_id"));
+				rowData.add(resultSet.getString("goods_name"));
+				rowData.add(""+resultSet.getDouble("goods_number"));
+				rowData.add(""+resultSet.getDouble("goods_volume"));
+				rowData.add(""+resultSet.getDouble("goods_weight"));
+				vData.add(rowData);
+			}
+
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
-		JLabel lblNewLabel_8 = new JLabel("方案6:100<体积<=1000,质量>1000");
-		lblNewLabel_8.setFont(new Font("微软雅黑", Font.PLAIN, 15));
-		lblNewLabel_8.setBounds(44, 455, 336, 27);
-		contentPane.add(lblNewLabel_8);
 		
-		JLabel lblNewLabel_9 = new JLabel("方案7:体积>1000,质量<=100");
-		lblNewLabel_9.setFont(new Font("微软雅黑", Font.PLAIN, 15));
-		lblNewLabel_9.setBounds(44, 480, 336, 27);
-		contentPane.add(lblNewLabel_9);
+		vData.add(rowData_1);
+		DefaultTableModel DTM=new DefaultTableModel(vData, rowName);
 		
-		JLabel lblNewLabel_10 = new JLabel("方案8:体积>1000,100<质量<=1000");
-		lblNewLabel_10.setFont(new Font("微软雅黑", Font.PLAIN, 15));
-		lblNewLabel_10.setBounds(44, 505, 336, 27);
-		contentPane.add(lblNewLabel_10);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(416, 51, 473, 347);
+		contentPane.add(scrollPane);
+		table = new JTable();
+		scrollPane.setViewportView(table);
 		
-		JLabel lblNewLabel_11 = new JLabel("方案9:体积>1000,质量>1000");
-		lblNewLabel_11.setFont(new Font("微软雅黑", Font.PLAIN, 15));
-		lblNewLabel_11.setBounds(44, 530, 336, 27);
-		contentPane.add(lblNewLabel_11);
-		
-		JLabel lblNewLabel_12 = new JLabel("特殊方案:如药品,化工产品等。");
-		lblNewLabel_12.setFont(new Font("微软雅黑", Font.PLAIN, 15));
-		lblNewLabel_12.setBounds(44, 555, 336, 27);
-		contentPane.add(lblNewLabel_12);
+		table.setModel(DTM);
 	}
 }
